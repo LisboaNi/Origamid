a695819ed900d7687797cda2d6ba749081478913
Nicoli Lisboa <nick.lisboa04@gmail.com>
2025-06-24 16:30:13 -0300

    .galeria

--- bikecraft/js/script.js
+++ bikecraft/js/script.js
@@ -43,23 +43,24 @@ function eventosPerguntas(pergunta) {
 
 perguntas.forEach(eventosPerguntas);
 
-// Galeria de Bicicletas
-const galeria = document.querySelectorAll(".bicicleta-imagens img");
-const galeriaContainer = document.querySelector(".bicicleta-imagens");
+document.addEventListener("DOMContentLoaded", function () {
+  const galeria = document.querySelectorAll(".bicicleta-imagens img");
+  const galeriaContainer = document.querySelector(".bicicleta-imagens");
 
-function trocarImagem(event) {
-  const img = event.currentTarget;
-  const media = matchMedia("(min-width: 1000px)").matches;
-  if (media) {
-    galeriaContainer.prepend(img);
+  function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia("(min-width: 1000px)").matches;
+    if (media) {
+      galeriaContainer.prepend(img);
+    }
   }
-}
 
-function eventosGaleria(img) {
-  img.addEventListener("click", trocarImagem);
-}
+  function eventosGaleria(img) {
+    img.addEventListener("click", trocarImagem);
+  }
 
-galeria.forEach(eventosGaleria);
+  galeria.forEach(eventosGaleria);
+});
 
 // Animação
 if (window.SimpleAnime) {
